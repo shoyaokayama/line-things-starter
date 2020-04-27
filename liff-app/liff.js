3574ac9dccb3cb088c60a844fcae09c4f16f51ba
shawyam <sho.edu.human@gmail.com>
2020-04-27 10:18:34 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'dd1ed191-7f05-4cba-9246-9ca2eb53662d'; // LED, Button
+const USER_SERVICE_UUID         = '6d78f1e4-6aea-41a2-b500-e6e44700cfcc'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
